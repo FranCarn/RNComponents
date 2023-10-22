e43295003184432ea809d31637cccbc80f0957a4
@FranCarn <carnevalefranco99@gmail.com>
2023-10-22 01:51:41 -0300

    adding navigate

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -3,14 +3,18 @@ import {Text, View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {MenuItem} from '../interfaces/appInterfaces';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   item: MenuItem;
 }
 
 export const FlatListMenuItem = ({item}: Props) => {
+  const {navigate} = useNavigation();
   return (
-    <TouchableOpacity activeOpacity={0.8}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigate(item.component as never)}>
       <View style={styles.container}>
         <Icon name={item.icon} color="gray" size={23} />
         <Text style={styles.itemText}>{item.name}</Text>
