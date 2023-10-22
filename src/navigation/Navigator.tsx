7707f77280319d8cad2f0a5d5bfe5df662ce6f56
@FranCarn <carnevalefranco99@gmail.com>
2023-10-22 01:52:01 -0300

    updating navigator

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/HomeScreen';
+import {Animation101} from '../screens/Animation101';
+import {Animation102} from '../screens/Animation102';
+
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Animation101" component={Animation101} />
+      <Stack.Screen name="Animation102" component={Animation102} />
     </Stack.Navigator>
   );
 };
