f8bcd0bba8a219ef80bc305f9dba8517a63072ca
@FranCarn <carnevalefranco99@gmail.com>
2023-10-23 15:16:33 -0300

    fade in & fade out ready

--- src/screens/Animation101.tsx
+++ src/screens/Animation101.tsx
@@ -1,12 +1,27 @@
 import React, {useRef} from 'react';
-import {Animated, StyleSheet, View} from 'react-native';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
 export const Animation101 = () => {
   const opacity = useRef(new Animated.Value(0)).current;
-
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(() => console.log('finish animation'));
+  };
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.purpleBox, opacity}} />
+      <Animated.View style={{...styles.purpleBox, opacity, marginBottom: 20}} />
+      <Button title="Fade in" onPress={fadeIn} />
+      <Button title="Fade out" onPress={fadeOut} />
     </View>
   );
 };
