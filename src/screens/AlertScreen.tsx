5a7722bcb8c4b6cd4664a32b99f8b8d03d868e68
@FranCarn <carnevalefranco99@gmail.com>
2023-10-29 15:15:05 -0300

    installing external package to manange prompts in android

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Alert, Button, View} from 'react-native';
+import prompt from 'react-native-prompt-android';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
@@ -20,14 +21,41 @@ export const AlertScreen = () => {
     );
   };
   const showPrompt = () => {
-    Alert.prompt('You sure?', 'This action cant revert', (value: string) =>
-      console.log('info: ', value),
+    // Alert.prompt(
+    //   'You sure?',
+    //   'This action cant revert',
+    //   (value: string) => console.log('info: ', value),
+    //   'secure-text',
+    //   '',
+    //   'numeric',
+    // );
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
     );
   };
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button title="Show Alert" onPress={showAlert} />
+      <View style={{height: 10}} />
       <Button title="Show Prompt" onPress={showPrompt} />
     </View>
   );
