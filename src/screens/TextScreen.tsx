dbbadc50212ed5dd8bee6c96258d1aa9d5270036
@FranCarn <carnevalefranco99@gmail.com>
2023-10-30 15:01:37 -0300

    multiple textinputs

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles as globalStyles} from '../theme/appTheme';
 
 export const TextScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+  const handleChange = (value: string, field: string) => {
+    setForm(prev => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
   return (
     <View style={globalStyles.globalMargin}>
       <HeaderTitle title="Text inputs" />
-      <TextInput style={styles.inputStyle} />
+      <TextInput
+        style={styles.inputStyle}
+        value={form.name}
+        placeholder="Your name"
+        autoCorrect={false}
+        autoCapitalize="words"
+        onChangeText={value => handleChange(value, 'name')}
+      />
+
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Your email"
+        value={form.email}
+        keyboardType="email-address"
+        autoCorrect={false}
+        autoCapitalize="none"
+        onChangeText={value => handleChange(value, 'email')}
+        keyboardAppearance="dark"
+      />
+
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Your phone"
+        value={form.phone}
+        keyboardType="phone-pad"
+        onChangeText={value => handleChange(value, 'phone')}
+      />
+
+      <HeaderTitle title={JSON.stringify(form, null, 3)} />
     </View>
   );
 };
@@ -19,5 +58,6 @@ const styles = StyleSheet.create({
     height: 50,
     paddingHorizontal: 10,
     borderRadius: 10,
+    marginVertical: 10,
   },
 });
