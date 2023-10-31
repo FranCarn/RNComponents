86cddfc23d098d25255bc92442896dc5a6ac01c4
@FranCarn <carnevalefranco99@gmail.com>
2023-10-31 17:47:53 -0300

    KeyboardAvoidingView ready

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -1,7 +1,16 @@
 import React, {useState} from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles as globalStyles} from '../theme/appTheme';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
 export const TextScreen = () => {
   const [form, setForm] = useState({
@@ -16,38 +25,45 @@ export const TextScreen = () => {
     }));
   };
   return (
-    <View style={globalStyles.globalMargin}>
-      <HeaderTitle title="Text inputs" />
-      <TextInput
-        style={styles.inputStyle}
-        value={form.name}
-        placeholder="Your name"
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={value => handleChange(value, 'name')}
-      />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={globalStyles.globalMargin}>
+            <HeaderTitle title="Text inputs" />
+            <TextInput
+              style={styles.inputStyle}
+              value={form.name}
+              placeholder="Your name"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => handleChange(value, 'name')}
+            />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <TextInput
+              style={styles.inputStyle}
+              placeholder="Your email"
+              value={form.email}
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={value => handleChange(value, 'email')}
+              keyboardAppearance="dark"
+            />
 
-      <TextInput
-        style={styles.inputStyle}
-        placeholder="Your email"
-        value={form.email}
-        keyboardType="email-address"
-        autoCorrect={false}
-        autoCapitalize="none"
-        onChangeText={value => handleChange(value, 'email')}
-        keyboardAppearance="dark"
-      />
-
-      <TextInput
-        style={styles.inputStyle}
-        placeholder="Your phone"
-        value={form.phone}
-        keyboardType="phone-pad"
-        onChangeText={value => handleChange(value, 'phone')}
-      />
-
-      <HeaderTitle title={JSON.stringify(form, null, 3)} />
-    </View>
+            <TextInput
+              style={styles.inputStyle}
+              placeholder="Your phone"
+              value={form.phone}
+              keyboardType="phone-pad"
+              onChangeText={value => handleChange(value, 'phone')}
+            />
+            <View style={{height: 100}} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
