531f5db80b9fac17d52e9d60ad982e4c79ac7840
@FranCarn <carnevalefranco99@gmail.com>
2023-11-08 16:38:59 -0300

    slide pagination ready

--- src/screens/SlideScreen.tsx
+++ src/screens/SlideScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   Image,
@@ -8,12 +8,13 @@ import {
   View,
 } from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 import {Slide, slideItems} from '../data/SlideShow-data';
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
 export const SlideScreen = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const renderItem = (item: Slide) => {
     return (
       <View style={styles.itemContainer}>
@@ -28,14 +29,24 @@ export const SlideScreen = () => {
     <SafeAreaView style={{flex: 1, paddingTop: 50}}>
       <HeaderTitle title="Slide Screen" />
       <Carousel
-        // ref={c => {
-        //   this._carousel = c;
-        // }}
         data={slideItems}
         renderItem={({item}) => renderItem(item)}
         sliderWidth={screenWidth}
         itemWidth={screenWidth}
         layout="default"
+        onSnapToItem={index => {
+          setActiveIndex(index);
+        }}
+      />
+      <Pagination
+        activeDotIndex={activeIndex}
+        dotsLength={slideItems.length}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 10,
+          backgroundColor: '#5856d6',
+        }}
       />
     </SafeAreaView>
   );
