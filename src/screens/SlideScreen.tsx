8a1e6ed4d15ee8bb8c88e940b1053b2a7d1ca317
@FranCarn <carnevalefranco99@gmail.com>
2023-11-08 16:51:02 -0300

    action buttons in slide ready

--- src/screens/SlideScreen.tsx
+++ src/screens/SlideScreen.tsx
@@ -6,10 +6,13 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
 } from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
 import {Slide, slideItems} from '../data/SlideShow-data';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
@@ -24,7 +27,7 @@ export const SlideScreen = () => {
       </View>
     );
   };
-
+  const {navigate} = useNavigation();
   return (
     <SafeAreaView style={{flex: 1, paddingTop: 50}}>
       <HeaderTitle title="Slide Screen" />
@@ -38,16 +41,41 @@ export const SlideScreen = () => {
           setActiveIndex(index);
         }}
       />
-      <Pagination
-        activeDotIndex={activeIndex}
-        dotsLength={slideItems.length}
-        dotStyle={{
-          width: 10,
-          height: 10,
-          borderRadius: 10,
-          backgroundColor: '#5856d6',
-        }}
-      />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginHorizontal: 20,
+        }}>
+        <Pagination
+          activeDotIndex={activeIndex}
+          dotsLength={slideItems.length}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 10,
+            backgroundColor: '#5856d6',
+          }}
+        />
+        {slideItems.length - 1 === activeIndex && (
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              backgroundColor: '#5856d6',
+              borderRadius: 10,
+              flexDirection: 'row',
+              height: 50,
+              justifyContent: 'center',
+              width: 130,
+            }}
+            activeOpacity={0.8}
+            onPress={() => navigate('Home')}>
+            <Text style={{fontSize: 20, color: '#fff'}}>Start!</Text>
+            <Icon name="chevron-forward-outline" color="#fff" size={35} />
+          </TouchableOpacity>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
