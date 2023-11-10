1b4ee12c421b508ccd0584000149ec3b4dd90ef3
@FranCarn <carnevalefranco99@gmail.com>
2023-11-10 17:24:38 -0300

    initial theme config

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+  Theme,
+} from '@react-navigation/native';
 import {Navigator} from './src/navigation/Navigator';
+
+const customTheme: Theme = {
+  dark: true,
+  colors: {
+    ...DarkTheme.colors,
+  },
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={customTheme}>
       <Navigator />
     </NavigationContainer>
   );
--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -3,7 +3,7 @@ import {Text, View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {MenuItem} from '../interfaces/appInterfaces';
 import {TouchableOpacity} from 'react-native-gesture-handler';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useTheme} from '@react-navigation/native';
 
 interface Props {
   item: MenuItem;
@@ -11,13 +11,16 @@ interface Props {
 
 export const FlatListMenuItem = ({item}: Props) => {
   const {navigate} = useNavigation();
+  const {colors} = useTheme();
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       onPress={() => navigate(item.component as never)}>
       <View style={styles.container}>
         <Icon name={item.icon} color="#5857d6" size={23} />
-        <Text style={styles.itemText}>{item.name}</Text>
+        <Text style={{...styles.itemText, color: colors.text}}>
+          {item.name}
+        </Text>
         <View style={{flex: 1}} />
         <Icon name="chevron-forward-outline" color="#5857d6" size={23} />
       </View>
