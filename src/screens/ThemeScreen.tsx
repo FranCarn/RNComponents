9ac59679240ef6d46f946e534918ca5a9c588610
@FranCarn <carnevalefranco99@gmail.com>
2023-11-12 14:49:53 -0300

    theme screen ready

--- src/screens/ThemeScreen.tsx
+++ src/screens/ThemeScreen.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {View} from 'react-native';
-import {styles} from '../theme/appTheme';
+import {StyleSheet, Text, View} from 'react-native';
+import {styles as globalStyles} from '../theme/appTheme';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export const ThemeScreen = () => {
   return (
-    <View style={styles.globalMargin}>
+    <View style={globalStyles.globalMargin}>
       <HeaderTitle title="Theme" />
+      <TouchableOpacity style={styles.toggleButton} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Ligth / Dark</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  toggleButton: {
+    backgroundColor: '#5856d6',
+    justifyContent: 'center',
+    width: 150,
+    height: 50,
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 17,
+  },
+});
