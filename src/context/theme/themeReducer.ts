06b6138031270f8b7a33db729df54b8bf63c1c07
@FranCarn <carnevalefranco99@gmail.com>
2023-11-13 16:36:35 -0300

    initial reducer config

--- src/context/theme/themeReducer.ts
+++ src/context/theme/themeReducer.ts
@@ -1,1 +1,38 @@
-export const themeReducer = () => {};
+import {Theme} from '@react-navigation/native';
+
+type ThemeAction = {type: 'light_theme'} | {type: 'dark_theme'};
+
+export interface ThemeState extends Theme {
+  currentTheme: 'dark' | 'light';
+  dividerColor: string;
+}
+
+const lightTheme: ThemeState = {
+  currentTheme: 'light',
+  dark: false,
+  colors: {
+    primary: 'red',
+    background: 'blue',
+    card: 'green',
+    text: 'pink',
+    border: 'orange',
+    notification: 'teal',
+  },
+  dividerColor: 'rgba(0,0,0,0.7)',
+};
+
+export const themeReducer = (
+  state: ThemeState,
+  action: ThemeAction,
+): ThemeState => {
+  switch (action.type) {
+    case 'light_theme':
+      return {...lightTheme};
+
+    case 'dark_theme':
+      return state;
+
+    default:
+      return state;
+  }
+};
